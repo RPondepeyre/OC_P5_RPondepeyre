156e7f5c49eeace1f517676bb371aab1133df2eb
RPondepeyre <remipondepeyre@gmail.com>
2022-04-01 14:19:22 +0200

    Model Firestation

---
+++ src/main/java/com/example/safetynet/model/Firestation.java
@@ -0,0 +1,24 @@
+package com.example.safetynet.model;
+
+public class Firestation {
+
+    private String adress;
+    private int station;
+
+
+    public String getAdress() {
+        return this.adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public int getStation() {
+        return this.station;
+    }
+
+    public void setStation(int station) {
+        this.station = station;
+    }
+}
